ed331161d08881c7023f20887ce15d0adf3f0001
Awadly <elawadas@whitman.edu>
2023-11-03 07:36:11 +0200

    models added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose=require('mongoose');
+
 
 require('dotenv').config();
 
@@ -9,6 +11,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.Atlas_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log("Mongo DB connected");
+    })
+    .catch((error) => {
+      console.error("Mongo DB connection error:", error);
+    });
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is  running on port: ${port}`);
 });
